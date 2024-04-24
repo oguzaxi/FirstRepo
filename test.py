d1a6786ca3d51b5f5346ee952ce1dae31bee67b2
oguz <168022379+oguzaxi@users.noreply.github.com>
2024-04-24 22:26:59 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print ("hello world")
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "CREEPY": "ürkütücü"
+            }
+            
+            
+word = input("anlamadığın bir kelimeyi benimle paylaş!")
+
+if word in meme_dict:
+    print(word, "kelimesinin anlamı =" , meme_dict[word])
+else:
+    print("maalasef verdiğiniz kelime listemde yok!!!")
